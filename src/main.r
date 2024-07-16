3b1b9967a97ad8ad2b6ac7132d2f1af4f6c0043e
jmwingenroth <jwing421@gmail.com>
2024-07-16 11:53:33 -0400

    Rename scripts

--- src/main.r
+++ src/main.r
@@ -1,15 +1,12 @@
 # Run scripts to generate all figures in NAFO Solar report
 
 run_all <- function() {
-    source("src/maps.r")
-    source("src/temporal.r")
-
-    # For running with a timer function such as `system.time()`
-    print("Total time taken:")
+    source("src/nlcd.r")
+    source("src/eia.r")
 }
 
 start <- Sys.time()
 run_all()
 end <- Sys.time()
-print("Runtime:")
-print(end - start)
+
+cat("Runtime:\n"); print(end - start)
